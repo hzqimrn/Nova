b66ea115e884c019d4e656f9e5bc54a68ee84bf1
hzqimrn <80167658+hzqimrn@users.noreply.github.com>
2021-04-04 21:50:05 +0800

    Add files via upload

---
+++ Blogr-page/js/main.js
@@ -0,0 +1,10 @@
+$("button.navbar-toggler").on("click", function (event) {
+   $(".icon").toggleClass("active");
+});
+
+// $("button#navButton1.btn").on("click", function () {
+
+//       $(".arrow-dark").toggleClass("rotate");
+//       $(".arrow-dark").toggleClass("reset");
+
+// });
